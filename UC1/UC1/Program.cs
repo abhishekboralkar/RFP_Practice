aff61aa5e34b6bc028a40dc8652c7dc99a1003b3
abhishekboralkar <boralkarabhishek@gmail.com>
2022-05-08 13:18:29 +0530

    [Abhishek]Add:Emp Pr_Or_Ab

---
+++ UC1/UC1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PracticeProblem1
+{
+    public class Program
+    {
+        public int EmpPresent = 1;
+        public void CheckEmpPresentAbsent()
+        {
+            Random Check = new Random();
+            int CheckEmp = Check.Next(0, 2);
+
+            if (EmpPresent == CheckEmp)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+        public static void Main(string[] args)
+
+        {
+            Program prg = new Program();
+            prg.CheckEmpPresentAbsent();
+
+        }
+    }
+}
