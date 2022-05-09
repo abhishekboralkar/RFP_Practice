86da50e18b485635bf744fc2007204999f3ef51c
abhishekboralkar <boralkarabhishek@gmail.com>
2022-05-09 13:50:19 +0530

    [Abhishek]Add:calculate wage

---
+++ UC2/Day2UC2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace PracticeProblem1
+{
+    public class Program
+    {
+        public int EmpPresent = 1;
+        public int Emphr = 8;
+        public int PartTime = 1;
+        public int WagesPerHour = 20;
+        public int FullTimeEmp= 1;
+        public void CheckEmpPresentAbsent()
+        {
+            Random Check = new Random();
+            int CheckEmp = Check.Next(0, 2);
+
+            if (EmpPresent == CheckEmp)
+            {
+                Console.WriteLine("Employee is Present");
+                Random Timecheck = new Random();
+                int checkTimeEmp = Timecheck.Next(0, 2);
+                if (FullTimeEmp == checkTimeEmp)
+                {
+                    int DailyWagePerHr = Emphr * WagesPerHour;
+                    Console.WriteLine("Dailywages" + DailyWagePerHr);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+        public static void Main(string[] args)
+
+        {
+            Program prg = new Program();
+            prg.CheckEmpPresentAbsent();
+
+        }
+    }
+}
